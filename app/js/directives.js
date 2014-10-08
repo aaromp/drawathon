12c0b91374df3509b7dee5e79e634c26ffe2350d
Aaron Ward <aaromp@gmail.com>
2014-10-08 11:38:07 -0700

    set up canvas event listeners

--- app/js/directives.js
+++ app/js/directives.js
@@ -11,21 +11,46 @@ angular.module('drawathon.directives', [])
   }])
 
   .directive('sketch', [function() {
-    var link, options;
+    var link, options, drawing;
 
     options = {
       height: 480,
       width: 640
     };
 
+    drawing = false;
+
+    var startDrawing, stopDrawing, draw;
+
+    startDrawing = function() {
+      console.log('starting to draw');
+      drawing = true;
+    };
+
+    stopDrawing = function() {
+      console.log('stoped drawing');
+      drawing = false;
+    };
+
+    draw = function(e) {
+      e.preventDefault();
+      console.log(e);
+    };
+
     link = function(scope, element, attributes) {
-      var canvas;
+      var canvas, context;
       canvas = element.find('canvas')[0];
+      context = canvas.getContext('2d');
 
       canvas.width = attributes.width || options.width;
       canvas.height = attributes.height || options.height;
 
-      // console.log(canvas.width, canvas.height);
+      canvas.onmousedown = startDrawing;
+      canvas.onmousemove = draw;
+
+      canvas.onmouseup = stopDrawing;
+      canvas.onmouseout = stopDrawing;
+
     };
 
     return {
