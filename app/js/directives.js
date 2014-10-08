5384e23d43d4206c9c1e112d3becb1dc27c95b21
Aaron Ward <aaromp@gmail.com>
2014-10-08 11:09:30 -0700

    add sketch directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,25 @@
 /* Directives */
 
 
-angular.module('drawathon.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('drawathon.directives', [])
+  .directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
+    };
+  }])
+
+  .directive('sketch', [function() {
+
+    var link;
+
+    link = function(scope, element, attributes) {
+      console.log(element.find('canvas')[0]);
+    };
+
+    return {
+      restrict: 'EA',
+      template: '<canvas></canvas>',
+      scope: {},
+      link: link
     };
   }]);
