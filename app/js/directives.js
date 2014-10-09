59065ab68d6d0146e14a091d265b1121a7b841c3
Aaron Ward <aaromp@gmail.com>
2014-10-09 13:43:29 -0700

    fix rectangle clear

--- app/js/directives.js
+++ app/js/directives.js
@@ -86,35 +86,39 @@ angular.module('drawathon.directives', [])
     var draw, clear;
 
     draw = function(snapshot) {
-      var coordinate = snapshot.name().split(', ');
+      var coordinate;
+
+      coordinate = snapshot.name().split(', ');
       this.context.fillStyle = snapshot.val();
       this.context.fillRect(parseInt(coordinate[0]) * this.pixels, parseInt(coordinate[1]) * this.pixels, this.pixels, this.pixels);
     };
 
     clear = function(snapshot) {
-      var coordinate = snapshot.name().split(':');
+      var coordinate;
+
+      coordinate = snapshot.name().split(', ');
       this.context.clearRect(parseInt(coordinate[0]) * this.pixels, parseInt(coordinate[1]) * this.pixels, this.pixels, this.pixels);
     };
 
     link = function(scope, element, attributes) {
       var canvas, context;
+
+      // initialize sketch element properties on scope
       scope.canvas = element.find('canvas')[0];
       scope.context = scope.canvas.getContext('2d');
-
       scope.pixels = attributes.pixels || options.pixels;
       scope.color = attributes.color || options.color;
-
       scope.canvas.width = attributes.width || options.width;
       scope.canvas.height = attributes.height || options.height;
 
+      // add DOM event listeners
       scope.canvas.onmousemove = sketch.bind(scope);
-  
       scope.canvas.onmousedown = handleMousedown.bind(scope);
       document.onmouseup = handleMouseup;
-
       scope.canvas.onmouseenter = handleMouseenter;
       scope.canvas.onmouseout = handleMouseout;
 
+      // add firebase event listeners
       sketchDataRef.on('child_added', draw.bind(scope));
       sketchDataRef.on('child_changed', draw.bind(scope));
       sketchDataRef.on('child_removed', clear.bind(scope));
